6edf60817cfbe522bb7aeb3b38690b95edb2c0f4
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-02-05 15:52:28 +0800

    util: script to rerun main.py after file change

--- watch.sh
+++ watch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 while true; do
+    clear
     echo -e '\n\n ------- watch mode ------- \n\n'
     python3 main.py
-    fswatch -1 shape_calculator.py main.py test_module.py
+    fswatch -1 prob_calculator.py main.py test_module.py
 done
